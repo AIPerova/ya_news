da5e862173fa5e5d507ae8ce0d889ad84759b993
Алёна Перова <perovaai31@yandex.ru>
2023-05-21 12:14:16 +0300

    Add pytest test_content

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
 import pytest
+
+from django.conf import settings
+from django.utils import timezone
 
 from news.models import Comment, News
 
@@ -31,3 +35,38 @@ def comment(news, author):
         text='Текст комментария',
     )
     return comment
+
+
+@pytest.fixture
+def id_for_comment(comment):
+    return comment.id,
+
+
+@pytest.fixture
+def id_for_news(news):
+    return news.id,
+
+
+@pytest.fixture
+def all_news():
+    today = datetime.today()
+    all_news = [News(title=f'Новость {index}',
+                     text='Просто текст.',
+                     date=today - timedelta(days=index))
+                for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)]
+    News.objects.bulk_create(all_news)
+    return all_news
+
+
+@pytest.fixture
+def all_comments(news, author):
+    now = timezone.now()
+    all_comments = []
+    for index in range(2):
+        comment = Comment.objects.create(news=news,
+                                         author=author,
+                                         text=f'Tекст {index}',)
+        comment.created = now + timedelta(days=index)
+        comment.save()
+        all_comments.append(comment)
+    return all_comments
